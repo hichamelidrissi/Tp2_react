aa08fcca16c515ebc5818aeebc00d5428ff022e5
hichamelidrissi <hisham.el.idrissi.7@gmail.com>
2023-11-07 18:16:07 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImgsSlides from './component/ImgsSlides';
+
+function App() {
+  return (
+    <div className="App">
+      <ImgsSlides/>
+    </div>
+  );
+}
+
+export default App;
